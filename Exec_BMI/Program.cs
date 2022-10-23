1334f3c115dbe0bdc996eb1cf8d5d782b6dfb4d4
User <User@DESKTOP-M776RP5>
2022-10-23 21:49:12 +0800

    加入專案檔案。

---
+++ Exec_BMI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入身高(公尺)：");
+            string height = Console.ReadLine();
+            float numheight = float.Parse(height);
+
+            Console.Write("請輸入體重(公斤)：");
+            string weight = Console.ReadLine();
+            int numweight = int.Parse(weight);
+
+            BMI Number = new BMI();
+            Number.CountHeight = numheight;
+            Number.CountWeight = numweight;
+            double BMI = Number.CountBMI();
+            Console.WriteLine("您的BMI為"+BMI.ToString("0.00"));
+        }
+    }
+    class BMI
+    {
+        private float height;
+        public float CountHeight
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+		private int weight;
+		public int CountWeight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public double CountBMI()
+        {
+            return weight / (height*height);
+        }
+
+
+    }
+}
